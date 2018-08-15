a1a41d11f83dd281273f3d76f7e0449277fe50b7
Samantha Guergenenov <samguergen@gmail.com>
2018-08-15 13:17:54 -0400

    try2

--- app.js
+++ app.js
@@ -21,12 +21,11 @@ app.use(express.static(__dirname + '/app'));
 app.use(session({secret: "Sam is awesome"}));
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 // app.use(function(req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
@@ -187,7 +186,13 @@ app.post('/sendmail', function(req, res){
   app.use(allPages, function(req, res){
     res.sendFile(__dirname + '/app/index.html');
   });
-
+  
+  
+  app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+  });
 
 app.listen(process.env.PORT || 13270);
 
