2a7903f851989bbdb4cce9dffb4c46bfbdbe8fe0
Samantha Guergenenov <samguergen@gmail.com>
2018-08-15 15:39:31 -0400

    continue replace itnlanier with itnamerica contact email

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.post('/sendmail', function(req, res){
         subject: req.body.subject, // Subject line   
         text: JSON.stringify(req.body.text), // plain text body
         attachments: [{path: req.body.pdf}],
-        bcc: 'info@itnlanier.org'
+        bcc: 'info@itnamerica.org;morgan.jameson@itnamerica.org'
     };
   }
   else if (req.body && req.body.html){
@@ -137,7 +137,7 @@ app.post('/sendmail', function(req, res){
         subject: req.body.subject, // Subject line   
         text: JSON.stringify(req.body.text), // plain text body
         html: req.body.html, // html body
-        bcc: 'info@itnlanier.org'
+        bcc: 'info@itnamerica.org;morgan.jameson@itnamerica.org'
     };
   } else {
     console.log('sending email with neither');
@@ -146,7 +146,7 @@ app.post('/sendmail', function(req, res){
         to: req.body.to, // list of receivers
         subject: req.body.subject, // Subject line   
         text: JSON.stringify(req.body.text), // plain text body
-        bcc: 'info@itnlanier.org'
+        bcc: 'info@itnamerica.org;morgan.jameson@itnamerica.org'
     };
   }
 
