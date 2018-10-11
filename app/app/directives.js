999bfbad6930e74e18e955310d6c01c0e1ea501a
Samantha Guergenenov <samguergen@gmail.com>
2018-10-11 14:04:31 -0400

    try7

--- app/app/directives.js
+++ app/app/directives.js
@@ -1,5 +1,21 @@
 var myApp = angular.module('myApp');
 
+myApp.directive('fileModel', ['$parse', function ($parse) {
+   return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+         var model = $parse(attrs.fileModel);
+         var modelSetter = model.assign;
+         
+         element.bind('change', function(){
+            scope.$apply(function(){
+               modelSetter(scope, element[0].files[0]);
+            });
+         });
+      }
+   };
+}]);
+
 myApp.directive('match', function($parse) {
     return {
         require: 'ngModel',
