13291b6999d94860aba2184daf28cbd83bb39080
Samantha Guergenenov <sguergenenov@itnamerica.org>
2018-11-07 13:16:10 -0500

    loading comments for affiliates but not posting or removing them

--- app/app/affiliateController.js
+++ app/app/affiliateController.js
@@ -2,6 +2,6 @@ var myApp = angular.module('myApp');
 
 myApp.controller('AffiliateCtrl', ['$scope', '$transitions', '$http', '$anchorScroll', '$location', '$stateParams', '$timeout', '$state', '$rootScope', '$window', 'FormService', '$sce', 'DataService', '$q',  function($scope, $transitions, $http, $anchorScroll, $location, $stateParams, $timeout, $state, $rootScope, $window, FormService, $sce, DataService, $q) {
     console.log('inside affiliate controller');
-    $scope.comments = $stateParams.filter;
+    $scope.affiliateObj = $stateParams.filter;
 
 }]);
