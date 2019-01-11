52ab570d97332510a14e26b4e440a64349fe93b5
Samantha Guergenenov <samguergen@gmail.com>
2019-01-11 15:51:17 -0500

    moving adjusttimeforcalendar in parsevariable service

--- app/app/controllers/calendarController.js
+++ app/app/controllers/calendarController.js
@@ -3,8 +3,7 @@ var myApp = angular.module('myApp');
 myApp.controller('CalendarCtrl', ['$scope', '$transitions', '$http', '$anchorScroll', '$location', '$stateParams', '$timeout', '$state', '$rootScope', '$window', 'FormService', '$sce', 'DataService', 'ParseVariablesService', '$q',  function($scope, $transitions, $http, $anchorScroll, $location, $stateParams, $timeout, $state, $rootScope, $window, FormService, $sce, DataService, ParseVariablesService, $q) {
     console.log('inside calendar controller');
     $scope.eventObj = {}
-    
-    // $scope.adjustTimeForCalendar = ParseVariablesService.adjustTimeForCalendar;
+    $scope.adjustTimeForCalendar = ParseVariablesService.adjustTimeForCalendar;
 
     //agenda here refers to a detailed day view of the monthly calendar after selecting day.
     $scope.initAgenda = function() {
@@ -104,38 +103,6 @@ myApp.controller('CalendarCtrl', ['$scope', '$transitions', '$http', '$anchorScr
         }
       }
     };
-
-    $scope.adjustTimeForCalendar = function(theTime) {
-      var time = theTime.replace(" ", "");
-      time = time.toUpperCase();
-      var adjustedTime = {
-        hour: 0,
-        min: 0
-      };
-       if (time.includes("AM") && time.includes(":")){
-          adjustedTime.hour = time.substr(0,time.indexOf(':'));
-          adjustedTime.min = time.substr(time.indexOf(':')+1,time.indexOf('AM')-2);
-      } else if (time.includes("AM")){
-          adjustedTime.hour = time.substr(0,time.indexOf('AM'));
-          adjustedTime.min = 0;
-      } else if (time.includes("PM") && time.includes(":")){
-          adjustedTime.hour = parseInt(time.substr(0,time.indexOf(':')));
-          if (adjustedTime.hour < 12){
-            adjustedTime.hour = adjustedTime.hour + 12;
-          }
-          adjustedTime.min = time.substr(time.indexOf(':')+1,time.indexOf('PM')-2);
-      } else if (time.includes("PM")){
-          adjustedTime.hour = parseInt(time.substr(0,time.indexOf('PM')));
-          if (adjustedTime.hour < 12){
-            adjustedTime.hour = adjustedTime.hour + 12;
-          }
-          adjustedTime.min = 0;
-      }
-      adjustedTime.hour = parseInt(adjustedTime.hour);
-      adjustedTime.min = parseInt(adjustedTime.min);
-      return adjustedTime;
-    };
-
 
     $scope.initMonthCalendar = function(){
       $('#calendar').fullCalendar({
